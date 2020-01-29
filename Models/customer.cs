7c64500dac4f1ee18e430c93b8980553f4b815ab
Mike Calvert <mc692617@ohio.edu>
2020-01-29 11:43:00 -0500

    Changes

---
+++ Models/customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mc692617_MIS4200.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int customerID { get; set; }
+        public int customerFirstName { get; set; }
+        public int customerLastName { get; set; }
+        public int email { get; set; }
+        public int phone { get; set; }
+        public int customerSince { get; set; }
+        public ICollection<Orders> Order { get; set; }
+    }
+}

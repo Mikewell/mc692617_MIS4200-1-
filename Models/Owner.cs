3b575e290f2287f629c14a6ce6ccd63408b6ca4a
Mike Calvert <mc692617@ohio.edu>
2020-02-10 11:23:25 -0500

    validators(broke)

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -10,11 +10,27 @@ namespace mc692617_MIS4200.Models
     {
         [Key]
         public int ownerID { get; set; }
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="First Name Required")]
+        [StringLength(20)]
         public string firstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name Required")]
+        [StringLength(20)]
         public string lastName { get; set; }
+        [Display(Name = "Email")]
+        [Required]
+        [EmailAddress(ErrorMessage ="Enter Email Address")]
         public string email { get; set; }
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(\d{3)\) |\d{3}-)\d{3}-\d{4}$", 
+            ErrorMessage ="(xxx) xxx-xxxx")]
         public string phone { get; set; }
+        [Display(Name = "Address")]
+        [Required]
         public string address { get; set; }
+     
 
         public ICollection <Pet> Pets { get; set; }
 
